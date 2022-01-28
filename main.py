ec4a83b8c182030ff5319c880840317e2c07ba1d
Bekzodergashev <81473754+bekzodergashev1@users.noreply.github.com>
2022-01-28 20:01:02 +0500

    first bot

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.update import Update
+import settings
+
+
+updater = Updater(token=settings.TELEGRAM_TOKEN)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Salom')
+    context.bot.send_message(chat_id=update.message.chat_id, text='Assalomu alaykum')
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+updater.idle()
---
+++ settings/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from .lockal_settings import *
+except ImportError:
+    pass
