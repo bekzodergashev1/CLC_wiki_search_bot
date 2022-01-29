992b48ddacb528bca6cbf79ddadd84ed18ccb8c8
Bekzodergashev <81473754+bekzodergashev1@users.noreply.github.com>
2022-01-29 11:21:34 +0500

    ..

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ updater = Updater(token=settings.TELEGRAM_TOKEN)
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('Salom')
-    context.bot.send_message(chat_id=update.message.id, text='Assalomu alaykum! \n'
+    context.bot.send_message(chat_id=update.message.chat_id, text='Assalomu alaykum! \n'
                                                                   'Wikipediya botiga xuh kelibsiz!\n'
                                                                   'Izlash uchun /search Amir temur')
 
